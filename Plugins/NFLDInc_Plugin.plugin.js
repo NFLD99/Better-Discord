31dc2824c94d06b32716246107a4f0b964f69184
NFLD99 <NFLD99@users.noreply.github.com>
2022-06-17 23:55:22 -0230

    Updated

--- Plugins/NFLDInc_Plugin.plugin.js
+++ Plugins/NFLDInc_Plugin.plugin.js
@@ -1,80 +1,122 @@
 //META{"name":"NFLDInc_Plugin"}*//
 
 class NFLDInc_Plugin {
-  getName() { return "NFLDInc_Plugin"; }
+    getName() { return "NFLDInc_Plugin"; }
 
-  getVersion() { return "7.0.0"; }
+    getVersion() { return "8.0.0"; }
 
-  getAuthor() { return "NFLD99"; }
+    getAuthor() { return "NFLD99"; }
 
-  getDescription() { return "NFLDInc_Plugin."; }
+    getDescription() { return "NFLDInc_Plugin. Fixed By Uri"; }
 
-  constructor() {
-    this.addElement = this.addElement.bind(this);
-  }
-
-  start() {
-    // If ZLib exists, check for updates
-    if (window.ZLibrary) ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin", this.getVersion(),"https://raw.githubusercontent.com/NFLD99/Better-Discord/master/Plugins/NFLDInc_Plugin.plugin.js")
-
-    const UserStore = BdApi.findModuleByProps("getCurrentUser");
-
-    const appMount = document.getElementById("app-mount");
-    appMount.classList.add("NFLDInc"); // add NFLDInc class to appmount
-    appMount.setAttribute("NFLDIncCurrentUser", UserStore.getCurrentUser().id); // Add current user to appmount
-  }
-
-  addElement(avatarElement, id) {
-	if (avatarElement.parentElement.querySelector(".NFLDIncChatUser")) return;
-    const newSibling = document.createElement("div");
-    newSibling.classList.add("NFLDIncChatUser");
-    newSibling.setAttribute("NFLDIncChatUser", id ? id : avatarElement.src.split("/")[4]);
-    avatarElement.parentElement.append(newSibling);
-  }
-
-  observer(mutation) {
-    if (!mutation.addedNodes.length || !(mutation.addedNodes[0] instanceof Element)) return;
-    const added = mutation.addedNodes[0];
-
-    const avatarInner = added.querySelectorAll(".avatar-VxgULZ");
-    if (avatarInner.length) {
-        // modal user
-        if (avatarInner.length == 1) {
-            const id = avatarInner[0].src.split("/")[4];
-            const modal = avatarInner[0].closest(".inner-1ilYF7");
-            if (modal) return modal.setAttribute("NFLDIncPopoutUser", id);
-        }
-
-        // For DevilBro or StatusEverywhere
-        for (let i = 0; i < avatarInner.length; i++) {
-			// chat class, member list class
-            const avatarOuter = avatarInner[i].closest(".avatar-1BDn8e, .avatar-3uk_u9");
-            if (avatarOuter) this.addElement(avatarOuter, avatarInner[i].src.split("/")[4]);
-        }
-        return; // To avoid the stuff below
+    constructor() {
+        this.addElement = this.addElement.bind(this);
     }
 
-    // Normal chat avatars
-    const avatarOuter = added.querySelectorAll(".avatar-1BDn8e");
-    if (avatarOuter.length) {
-        for (let i = 0; i < avatarOuter.length; i++) this.addElement(avatarOuter[i]);
+    start() {
+        if (window.ZLibrary) ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin", this.getVersion(), "")
+        const UserStore = BdApi.findModuleByProps("getCurrentUser");
+        const appMount = document.getElementById("app-mount");
+        appMount.classList.add("NFLDInc");
+        appMount.setAttribute("NFLDIncCurrentUser", UserStore.getCurrentUser().id);
     }
-	
-	// Do the toasts
-	if (window.XenoLib) {
-		const toast = added.querySelector(".toast-text") || added.querySelector(".bd-toast");
-		if (toast) {
-			toast.style.display = "none";
-			XenoLib.Notifications.danger(toast.textContent, {timeout: 3000});
-		}
-	}
-  }
 
-  stop() {
-    const appMount = document.getElementById("app-mount");
-    appMount.classList.remove("NFLDInc"); // remove NFLDInc class to appmount
-    appMount.removeAttribute("NFLDIncCurrentUser"); // remove current user to appmount
-    const allChatUserElements = document.querySelectorAll(".NFLDIncChatUser");
-    for (let i = 0; i < allChatUserElements.length; i++) allChatUserElements[i].remove();
-  }
+    addElement(avatarElement, id) {
+        if (avatarElement.parentElement.querySelector(".NFLDIncChatUser")) return;
+        const newSibling = document.createElement("div");
+        newSibling.classList.add("NFLDIncChatUser");
+        newSibling.setAttribute("NFLDIncChatUser", id ? id : avatarElement.src.split("/")[4]);
+        avatarElement.parentElement.append(newSibling);
+    }
+
+    addAttribute(elem, attributeName, id) {
+        if (elem !== null) {
+            return elem.setAttribute(attributeName, id);
+        }
+    }
+
+    observer(mutation) {
+        if (!mutation.addedNodes.length || !(mutation.addedNodes[0] instanceof Element)) return;
+        const added = mutation.addedNodes[0];
+
+        const userPopoutClass = ".userPopout-2j1gM4";
+        const userModalClass = ".root-8LYsGj";
+        const membersClass = ".member-2gU6Ar";
+        const channelsClass = ".channel-1Shao0";
+        const messagesClass = ".message-2CShn3";
+        const friendsClass = ".peopleListItem-u6dGxF";
+
+        const avatarClass = ".avatar-b5OQ1N";
+        const avatarClassInMessages = ".avatar-2e8lTP";
+
+        const allAvatars = added.querySelectorAll(avatarClass);
+
+        let avatarID = null;
+
+        if (allAvatars.length) {
+            for (let i = 0; i < allAvatars.length; i++) {
+                const elems = allAvatars[i].closest(friendsClass);
+
+                avatarID = allAvatars[i].src.split("/")[4];
+
+                const popoutElem = allAvatars[i].closest(userPopoutClass);
+                this.addAttribute(popoutElem, "NFLDIncPopoutUser", avatarID);
+
+                const modalElem = allAvatars[i].closest(userModalClass);
+                this.addAttribute(modalElem, "NFLDIncModalUser", avatarID);
+
+                const memberElem = allAvatars[i].closest(membersClass);
+                this.addAttribute(memberElem, "NFLDIncMemberUser", avatarID);
+
+                const channelElem = allAvatars[i].closest(channelsClass);
+                this.addAttribute(channelElem, "NFLDIncChannelUser", avatarID);
+
+                const friendElem = allAvatars[i].closest(friendsClass);
+                this.addAttribute(friendElem, "NFLDIncFriendUser", avatarID);
+            }
+
+            return;
+        }
+
+        // messages
+        const avatarInner = added.querySelectorAll(avatarClassInMessages);
+        if (avatarInner.length) {
+            for (let i = 0; i < avatarInner.length; i++) {
+                const avatarOuter = avatarInner[i].closest(messagesClass);
+                avatarID = avatarInner[i].src.split("/")[4];
+                if (avatarOuter) {
+                    this.addElement(avatarOuter, avatarID);
+                    avatarOuter.setAttribute("NFLDIncChatUser", avatarID);
+                }
+            }
+            return;
+        }
+
+
+        if (window.XenoLib) {
+            const toast = added.querySelector(".toast-text") || added.querySelector(".bd-toast");
+            if (toast) {
+                toast.style.display = "none";
+                XenoLib.Notifications.danger(toast.textContent, { timeout: 3000 });
+            }
+        }
+    }
+
+    stop() {
+        const appMount = document.getElementById("app-mount");
+        appMount.classList.remove("NFLDInc");
+        appMount.removeAttribute("NFLDIncCurrentUser");
+        const allPopoutUserElements = document.querySelectorAll("[NFLDIncPopoutUser]");
+        for (let i = 0; i < allPopoutUserElements.length; i++) allPopoutUserElements[i].removeAttribute("NFLDIncPopoutUser");
+        const allModalUserElements = document.querySelectorAll("[NFLDIncModalUser]");
+        for (let i = 0; i < allModalUserElements.length; i++) allModalUserElements[i].removeAttribute("NFLDIncModalUser");
+        const allMemberUserElements = document.querySelectorAll("[NFLDIncMemberUser]");
+        for (let i = 0; i < allMemberUserElements.length; i++) allMemberUserElements[i].removeAttribute("NFLDIncMemberUser");
+        const allChannelUserElements = document.querySelectorAll("[NFLDIncChannelUser]");
+        for (let i = 0; i < allChannelUserElements.length; i++) allChannelUserElements[i].removeAttribute("NFLDIncChannelUser");
+        const allChatUserElements = document.querySelectorAll("[NFLDIncChatUser]");
+        for (let i = 0; i < allChatUserElements.length; i++) allChatUserElements[i].removeAttribute("NFLDIncChatUser");
+        const allFriendUserElements = document.querySelectorAll("[NFLDIncFriendUser]");
+        for (let i = 0; i < allFriendUserElements.length; i++) allFriendUserElements[i].removeAttribute("NFLDIncFriendUser");
+    }
 }
